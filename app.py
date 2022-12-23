3f0f6ca96c60483842da2162a7b0a7359976d723
lekau <lekau9222@gmail.com>
2022-12-23 12:45:14 +0200

    set up the new provider using oauth2 and oauthlib

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,redirect, url_for, session
 from flask_cors import CORS
+from flask_oauthlib.client import OAuth
+
 
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = 'sdvhkhdfbvxkjdfbkvdfbvkdvf'
+oauth = OAuth(app)
+
+nerdsSSOProvider = oauth.remote_app(
+    'nerdsSSOProvider',
+    consumer_key='acsac',
+    consumer_secret='thhsfsdafa',
+    request_token_params={'scope': 'username'},
+    base_url='http://localhost:5000/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='http://localhost:5000/oauth/token',
+    authorize_url='http://localhost:5000/oauth/authorize'
+)
+
+@nerdsSSOProvider.tokengetter
+def get_oauth_token():
+    return session.get('my_oauth_token')
+
 
 @app.route('/')
 def index():
@@ -10,7 +31,7 @@ def index():
 
 @app.route('/login')
 def login():
-    return "Login route endpoint"
+    return "Login route endpoint"  
 
 @app.route('/logout')
 def logout():
@@ -24,6 +45,6 @@ def signup():
 def change_password():
     return "Change password route endpoint"
 
-@app.route('/check-session')
+@app.route('/check-auth')
 def check_auth():
-    return "Check session route endpoint"
+    return "Check auth route endpoint"
